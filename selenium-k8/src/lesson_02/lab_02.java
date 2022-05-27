a0d2ff4c04da56b652dff63d775573ce615a055c
VyDang-Ivy <vy.dang.ldo1@gmail.com>
2022-05-27 10:35:03 +0700

    Lab 01

---
+++ selenium-k8/src/lesson_02/lab_02.java
@@ -0,0 +1,12 @@
+package lesson_02;
+
+public class lab_02 {
+
+    public static void main(String[] args) {
+        double d = 1.1;
+        float f = 1.1F;
+
+        float randomF = 18.5f + f;
+
+    }
+}
