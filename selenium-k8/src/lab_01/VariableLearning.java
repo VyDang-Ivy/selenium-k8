c3ce9d8c1f970d932c32249773ceadaf27aa290b
VyDang-Ivy <vy.dang.ldo1@gmail.com>
2022-05-27 17:13:03 +0700

    Lab 01

---
+++ selenium-k8/src/lab_01/VariableLearning.java
@@ -0,0 +1,9 @@
+package lab_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        byte myBytenum;
+        myBytenum = 127;
+        System.out.println(myBytenum);
+    }
+}
