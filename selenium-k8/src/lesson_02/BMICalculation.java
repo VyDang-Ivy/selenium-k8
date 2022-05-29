05c584bd05a957c68c803c4e58ff1c2a70687dd6
VyDang-Ivy <vy.dang.ldo1@gmail.com>
2022-05-29 14:22:08 +0700

    Lab 02

---
+++ selenium-k8/src/lesson_02/BMICalculation.java
@@ -0,0 +1,38 @@
+package lesson_02;
+
+import java.util.Scanner;
+
+public class BMICalculation {
+
+    public static void main(String[] args) {
+
+    // <18.5 || 18.5 - 24.9 || 25 - 29.9 || >=30
+        Scanner scanner = new Scanner(System.in);
+        final double BMI_NORMAL_WEIGHT_MIN = 18.5;
+        final double BMI_NORMAL_WEIGHT_MAX = 24.9;
+
+        System.out.print("Please input your height(m): ");
+        double yourHeight = scanner.nextDouble();
+
+        System.out.print("Please input your weight(kg): ");
+        double yourWeight = scanner.nextDouble();
+
+        double normalWeightMin = BMI_NORMAL_WEIGHT_MIN * (yourHeight * yourHeight);
+        double normalWeightMax = BMI_NORMAL_WEIGHT_MAX * (yourHeight * yourHeight);
+
+        double bmi = yourWeight / (yourHeight * yourHeight);
+
+        if (bmi < 18.5) {
+            System.out.println("You are underweight!");
+            System.out.println("You should increase your weight: " + (normalWeightMin - yourWeight));
+        } else if (bmi < 25) {
+            System.out.println("You are normal weight!");
+        } else if (bmi < 30) {
+            System.out.println("You are overweight!");
+            System.out.println("You should decrease your weight: " + (yourWeight - normalWeightMax));
+        } else {
+            System.out.println("You are obesity!");
+            System.out.println("You should decrease your weight: " + (yourWeight - normalWeightMax));
+        }
+    }
+}
